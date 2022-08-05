482bdc549fec4678a684f5a911a8f943c106bd79
BuildTools <unconfigured@null.spigotmc.org>
2022-08-04 20:21:07 -0600

    Added more code from the Canvas page

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
 let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
 weeklyExpenses = weeklyExpenses + answer
+
+answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
 
 for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
