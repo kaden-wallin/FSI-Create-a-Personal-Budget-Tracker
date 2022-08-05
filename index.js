130506ae35c002960d2acb86d37120682c5a9178
BuildTools <unconfigured@null.spigotmc.org>
2022-08-04 20:12:21 -0600

    Copy and pasted the assignment into my VSCode

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+
